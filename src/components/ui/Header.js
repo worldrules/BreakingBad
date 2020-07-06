ba54fc2d8c5cd21b555212f4baacd319f75594b6
unknown <worldrulesdev@gmail.com>
2020-07-06 10:35:09 -0300

    Header component Start

---
+++ src/components/ui/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../../img/logo.png';
+
+export const Header = () => {
+  return (
+    <header className="center">
+      <img src={logo} alt="Logo from Breaking Bad" />
+    </header>
+  );
+};
+export default Header;
