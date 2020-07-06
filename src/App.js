672d4db4445e25383eb86022bd872e89499629cf
unknown <worldrulesdev@gmail.com>
2020-07-06 10:58:02 -0300

    Axios Request and State

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Header from './components/ui/Header';
 import './App.css';
 
 const App = () => {
+  const [items, setItens] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItens = async () => {
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters`
+      );
+
+      console.log(result.data);
+      setItens(result.data);
+      setIsLoading(false);
+    };
+
+    fetchItens();
+  }, []);
+
   return (
     <div className="Container">
       <Header></Header>
